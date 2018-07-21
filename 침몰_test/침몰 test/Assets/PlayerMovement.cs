1cf95228499f537ee89b5de7e49321efc6b0eb83
minie12 <30361753+minie12@users.noreply.github.com>
2018-07-21 22:43:14 +0900

    rock 충돌 및 hp바
    07/21

---
+++ 침몰_test/침몰 test/Assets/PlayerMovement.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	public CharacterController2D controller;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
+
+    public Slider Healthbar;
+
+    public bool rockCollide = false;
+
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        MaxHealth = 200f;
+
+        CurrentHealth = MaxHealth;
+        Healthbar.value = CalculateHealth();
+    }
+	// Update is called once per frame
+	void Update () {
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+
+    }
+
+
+    float CalculateHealth()
+    {
+        return CurrentHealth / MaxHealth;
+
+    }
+
+    public void DealDamage(float damageValue)
+    {
+
+        CurrentHealth -= damageValue;
+        Healthbar.value = CalculateHealth();
+
+        if (CurrentHealth <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        CurrentHealth = 0;
+        Debug.Log("You dead.");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) { 
+      
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+    }
+
+    void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
+}
---
+++ 침몰_test/침몰 test/Assets/Script/RockAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockAttack : MonoBehaviour {
+
+    private Player player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+	}
+
+    void OnTriggerEnter2D(Collider2D rock)
+    {
+        if (rock.CompareTag("Player")) {
+            player.DealDamage(5);
+            Destroy(gameObject);
+        }
+    }
+            
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   
+}
