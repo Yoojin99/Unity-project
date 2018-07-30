d2766232b7642506bd1fd39f3e53d88054c2da8b
Yoojin99 <crm05265@gmail.com>
2018-07-30 17:26:49 +0900

    dd

---
+++ 침몰 poison/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+
+    public Slider Healthbar;
+    public Slider Poisonbar;
+    public Slider None;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- 침몰 test/Assets/Script/RockAttack.cs
+++ 침몰 test/Assets/Script/RockAttack.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class RockAttack : MonoBehaviour {
 
     private Player player;
+    Rigidbody2D rb;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        rb = GetComponent<Rigidbody2D>();
 
-	}
+    }
 
-    void OnTriggerEnter2D(Collider2D rock)
+
+    void OnTriggerEnter2D(Collider2D col)
     {
-        if (rock.CompareTag("Player")) {
+        if (col.gameObject.name.Equals("Player"))
+            rb.isKinematic = false;
+        
+    }
+    void OnCollisionEnter2D(Collision2D rock)
+    {
+        if (rock.gameObject.name.Equals("Player")) {
             player.DealDamage(5);
             Destroy(gameObject);
         }
+        /*
+        if (rock.CompareTag("Untagged")) {
+           
+        }*/
     }
             
 	
